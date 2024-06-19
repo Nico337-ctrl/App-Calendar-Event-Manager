84895cdb930517372017387452c5e2fa60757958
Nicolas Ojeda Correa <nojedac_24@hotmail.com>
2024-06-19 16:36:00 -0500

    se agrego script de envio de emails, .env

---
+++ notifications/emails/send_email.py
@@ -0,0 +1,33 @@
+from email.message import EmailMessage
+import smtplib
+import os
+from dotenv import load_dotenv
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+load_dotenv()
+# Configuración de Mailtrap
+remitente = os.getenv('USER')
+destinatario = 'samirdelportillo01@gmail.com'
+asunto = 'testeando'
+
+msg = MIMEMultipart()
+
+email = EmailMessage()
+email['From'] = remitente
+email['To'] = destinatario
+email['Subject'] = 'email test'
+
+with open('email.html', 'r') as archivo:
+    html = archivo.read()
+
+msg.attach(MIMEText(html, 'html'))
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+
+server.starttls()
+server.login(remitente, os.getenv('PASS'))
+
+server.sendmail(remitente, destinatario, msg.as_string())
+server.quit()
--- notifications/send_notification.py
+++ notifications/send_notification.py
@@ -7,8 +7,12 @@ def enviarNotificacion(mensaje, titulo):
     notificacion = Notify()
     notificacion.title = titulo
     notificacion.message = mensaje
-    notificacion.icon = "C:\Workspace\App EventosCalendario\calendeventmanager\static\images\logo-del-sena-01.png"
+    # notificacion.icon = "D:\Workspace\Nicolas\App-Calendar-Event-Manager\calendeventmanager\static\src\images\logo-del-sena-01.png"
     notificacion.send()
     # icono = "logo-del-sena-01.png"
     # ruta = path.abspath(path.dirname(__file__))
     # notificacion.icon = path.join(ruta, icono)
+
+
+#ejemplo de envio de notificacion    
+# enviarNotificacion('Este es una notificacion push ', 'Notificacion emergente')
