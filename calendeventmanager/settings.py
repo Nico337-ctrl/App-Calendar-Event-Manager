71fb471fa844c6a96c78f234d7fd728d5000e6eb
Nicolas Ojeda Correa <nojedac_24@hotmail.com>
2024-09-04 20:48:03 -0500

    cambiando motor de base de datos del proyecto (ahora es mysql)

--- calendeventmanager/settings.py
+++ calendeventmanager/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'calendeventmanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'dashboardeventos',
+            'USER': 'root',
+            'PASSWORD': 'root',
+            'HOST': 'localhost',
+            'PORT': '3306',
     }
 }
 
