b944bd94f8448eb61e9ccd8392578c2f5de879cc
Nicolas Ojeda Correa <nojedac_24@hotmail.com>
2024-09-20 15:16:32 -0500

    arreglos a los signals de usuarios y roles

--- dashboard/signals/usuarios/usuarios_signals.py
+++ dashboard/signals/usuarios/usuarios_signals.py
@@ -4,7 +4,7 @@ from dashboard.models.usuarios import User
 from dashboard.models.registros import Registros, TipoRegistro, EntidadRegistro
 from django.forms.models import model_to_dict
 from datetime import datetime
-
+from django.core.files import File
 
 
 def convert_datetime_fields(data):
@@ -14,6 +14,18 @@ def convert_datetime_fields(data):
             data[field] = value.isoformat()  # Convertir a cadena en formato ISO 8601
     return data
 
+def convert_image_fields(data):
+    """Convierte los campos de imagen en URLs o nombres de archivo."""
+    for field, value in data.items():
+        if isinstance(value, File):  # Verifica si el campo es un archivo
+            data[field] = value.url if hasattr(value, 'url') else value.name
+    return data
+
+def convert_fields(data):
+    """Convierte tanto los campos datetime como los de imagen."""
+    data = convert_datetime_fields(data)  # Convierte los datetime
+    data = convert_image_fields(data)     # Convierte las imágenes
+    return data
 
 @receiver(pre_save, sender=User)
 # Verificar una instancia anterior a la creada
@@ -25,7 +37,7 @@ def cache_old_instance(sender, instance, **kwargs):
 
 
 @receiver(post_save, sender=User)
-def registrar_evento(sender, instance, created, **kwargs):
+def registrar_usuario(sender, instance, created, **kwargs):
     tipo_accion = 'CREACION' if created else 'ACTUALIZACION'
     tipo_registro = TipoRegistro.objects.get(accion=tipo_accion)
     usuario = instance  # Aquí debes usar la instancia de `User`, no el ID
@@ -35,7 +47,7 @@ def registrar_evento(sender, instance, created, **kwargs):
     
     if created:
         # Registra la creación del usuario (mmodel: User)
-        valor_nuevo = convert_datetime_fields(model_to_dict(instance))
+        valor_nuevo = convert_fields(model_to_dict(instance))
         Registros.objects.create(
             tipo=tipo_registro,
             usuario=usuario,
@@ -49,8 +61,19 @@ def registrar_evento(sender, instance, created, **kwargs):
         # Registrando la actualizacion del usuario (model:User)
         old_instance = getattr(instance, '_old_instance', None)
         if old_instance:
-            old_data = convert_datetime_fields(model_to_dict(old_instance))
-            new_data = convert_datetime_fields(model_to_dict(instance))
+            old_data = convert_fields(model_to_dict(old_instance))
+            new_data = convert_fields(model_to_dict(instance))
+            
+            campos_modificados = []
+            valor_anterior = {}
+            valor_nuevo = {}
+            
+            for field in old_data:
+                if old_data[field] != new_data[field]:
+                    campos_modificados.append(field)
+                    valor_anterior[field] = old_data[field]
+                    valor_nuevo[field] = new_data[field]
+            
             for field in old_data:
                 if old_data[field] != new_data[field]:
                     Registros.objects.create(
@@ -58,20 +81,20 @@ def registrar_evento(sender, instance, created, **kwargs):
                         usuario=usuario,
                         entidad=entidad,
                         elemento_id=instance.id,
-                        campo_modificado=field,
+                        campo_modificado=', '.join(campos_modificados),
                         valor_anterior=old_data[field],
                         valor_nuevo=new_data[field],
                     )
 
 @receiver(post_delete, sender=User)
-def registrar_evento_eliminado(sender, instance, **kwargs):
+def registrar_usuario_eliminado(sender, instance, **kwargs):
     tipo_registro = TipoRegistro.objects.get(accion='ELIMINAR')
     usuario = instance  # Aquí también debes usar la instancia de `User`
     entidad, _ = EntidadRegistro.objects.get_or_create(
         nombre_entidad='Usuarios',
     )
     # Registrando la eliminacion del usuario (model:User)
-    valor_anterior = convert_datetime_fields(model_to_dict(instance))
+    valor_anterior = convert_fields(model_to_dict(instance))
     Registros.objects.create(
         tipo=tipo_registro,
         usuario=usuario,
