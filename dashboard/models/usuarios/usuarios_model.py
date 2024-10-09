9c85d76ca892d766289c556f803f0d2a9220585b
Alessandro <Aless.h.m.m@gmail.com>
2024-10-09 17:03:39 -0500

    Modificacion de botones y perfil responsive

--- dashboard/models/usuarios/usuarios_model.py
+++ dashboard/models/usuarios/usuarios_model.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.core.validators import RegexValidator
 
 class User(AbstractUser):
-        
+
+    imagen = models.ImageField(upload_to='perfiles/', blank=True, null=True)    
     email = models.EmailField('Correo electrónico', max_length=250, unique=True)
     telefono = models.CharField(
         'Teléfono', 
